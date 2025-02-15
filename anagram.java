9ebb478bdc63c7cab6b5c21c5039930e35f9d3fa
Ujjawalsingh <beingujjawalsingh@gmail.com>
2025-02-15 09:33:34 +0530

    Add files via upload

---
+++ anagram.java
@@ -0,0 +1,22 @@
+public class anagram {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+        if (str1.length() != str2.length()) {
+            System.out.println("The strings are not anagrams.");
+            return;
+        }
+        int[] count = new int[256];
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i)]++;
+            count[str2.charAt(i)]--;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (count[i] != 0) {
+                System.out.println("The strings are not anagrams.");
+                return;
+            }
+        }
+        System.out.println("The strings are anagrams.");
+    }
+}
